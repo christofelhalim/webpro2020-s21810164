134c561c593e3a2f39c1636acf7752fd4b89fef7
christofelhalim <s21810164@student.unklab.ac.id>
2020-11-17 16:08:17 +0800

    Pertemuan24

--- Pertemuan 21/src/App.js
+++ Pertemuan 21/src/App.js
@@ -1,23 +1,73 @@
-import "./App.css"; 
+import React, { Component, useEffect, useState} from "react";
 import Card from "./components/Card";
-import {person} from './Utils/person';
+import "./App.css";
+import axios from "axios";
 
 
-function App() {
-  return (
-   <>
-       {person.map((item) => {
+/*menggunakan class
+class App extends Component{
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((json) => this.setState({users: json}));
+   /* axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response => this.setState({users:response.data}));
+    
+  }
+
+  render(){
+    return (
+      <>
+      {this.state.users.map((item) => {
           return (
             <Card
-              name= {item.name}
-              institution= {item.institution}
-              address= {item.address}
-              phoneNumber= {item.phoneNumber}
+            name={item.name} 
+            username={item.username}
+            email={item.email}
+            phone={item.phone}
           />
           );
-       })}
-       </>
-  );
+          })}
+          </>
+    );
+  }
 }
+
+export default App;
+
+*/
+
+//dibawah ini functional componen
+
+const App = ()=> {
+
+  const[users,setUsers] = useState([]);
+
+  useEffect(()=>{
+
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => setUsers(response.data));
+  }, []);
+  return (
+    <>
+    {users.map((item)=> {
+        return (
+          <Card 
+            name={item.name}
+            username={item.username}
+            email={item.email}
+            phone={item.phone}
+            />
+        );
+      })}
+    </>
+  );
+};
 
 export default App;
